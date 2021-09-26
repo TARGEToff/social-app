4a38b20fefc2c54ab563b53d2f2c0e8352d5ec43
Filip Glura <lockontargt@gmail.com>
2021-09-26 17:07:19 +0200

    feat: added Post component and home page

---
+++ components/Post.js
@@ -0,0 +1,13 @@
+const Post = () => (
+    <div className="w-full sm:w-4/6 xl:w-4/12 flex flex-col items-center justify-center">
+        <div className="shadow-lg text-left rounded-t-2xl mt-6 w-full h-40 p-6">
+            <p>some text</p>
+        </div>
+        <div className="flex flex-row-reverse items-center p-8 shadow-lg rounded-b-2xl w-full h-12 bg-gray-100">
+            <button className="text-white bg-blue-500 w-20 h-8 rounded-xl">Send</button>
+        </div> 
+    </div>
+
+)
+
+export { Post }
--- pages/home.js
+++ pages/home.js
@@ -3,7 +3,7 @@ import { Switch } from "@headlessui/react";
 import { useDarkMode } from "hooks/useDarkMode";
 import { Post } from "components/Post";
 
-export default function LogIn() {
+export default function Home() {
     const [theme, setTheme] = useDarkMode();
     return (
         <div className="dark:bg-gray-800 flex flex-col items-center justify-center min-h-screen py-2">
@@ -32,9 +32,8 @@ export default function LogIn() {
                 </Switch>
             </header>
             <main className="flex flex-col items-center mt-6 w-full flex-1 px-10 text-center">
-                <div className="bg-gray-100 dark:bg-gray-600 rounded-xl h-96 w-96 flex flex-col justify-center items-center">
-                    <button className="text-white bg-blue-500 w-48 h-8 rounded-xl">Sign In with google</button>
-                </div>
+                    <Post />
+                    <Post />
             </main>
         </div>
     );
